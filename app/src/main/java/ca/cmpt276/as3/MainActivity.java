5d0f66fe55d2ecedd1b1cda731fcecce55376277
Gahee Kim <gaheekimedu@gmail.com>
2022-10-20 23:22:44 -0700

    Add animation to welcome screen

--- app/src/main/java/ca/cmpt276/as3/MainActivity.java
+++ app/src/main/java/ca/cmpt276/as3/MainActivity.java
@@ -6,8 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.widget.Toolbar;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +28,32 @@ public class MainActivity extends AppCompatActivity {
 
         // Button to main menu
         findViewById(R.id.btnMainMenu).setOnClickListener(v -> onRegisterClick());
+
+        TextView title = findViewById(R.id.tvTitle);
+        ImageView apple1 = findViewById(R.id.imgApple1);
+        ImageView apple2 = findViewById(R.id.imgApple2);
+
+        // ANIMATION (cite: https://github.com/daimajia/AndroidViewAnimations)
+        fadeAnimation(title);
+        bounceAnimation(apple1);
+        bounceAnimation(apple2);
+
+
+
+
+    }
+
+    private void fadeAnimation(TextView text) {
+        YoYo.with(Techniques.FadeIn)
+                .duration(2000)
+                .playOn(text);
+    }
+
+    private void bounceAnimation(ImageView img) {
+        YoYo.with(Techniques.Bounce)
+                .duration(700)
+                .repeat(10)
+                .playOn(img);
     }
 
     private void onRegisterClick() {
